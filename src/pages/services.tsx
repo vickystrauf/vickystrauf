929a9ee721a2a47fa3e2f4887e5cd1ddd01aa41e
Strauf <f.strauf@gmail.com>
2019-09-05 15:47:10 +1000

    spellcheck

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -41,13 +41,13 @@ const Services = () => {
     grid-gap: 50px;
     grid-template-rows: 2;
     justify-content: center;
-    
-  @media (max-width: ${props => props.theme.breakpoints[2]}) {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+
+    @media (max-width: ${props => props.theme.breakpoints[2]}) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
     }
-  `
+  `;
   const LogoBox = styled.div`
     width: 290px;
     height: 250px;
@@ -56,11 +56,11 @@ const Services = () => {
     align-items: center;
     background-image: url(${logo});
     background-size: cover;
-   
-    @media (max-width: ${props => props.theme.breakpoints[2]}) { 
+
+    @media (max-width: ${props => props.theme.breakpoints[2]}) {
       margin-block-end: 2rem;
     }
-  `
+  `;
   const BusinessBox = styled.div`
     width: 290px;
     height: 250px;
@@ -70,10 +70,10 @@ const Services = () => {
     background-image: url(${business});
     background-size: cover;
 
-    @media (max-width: ${props => props.theme.breakpoints[2]}) { 
+    @media (max-width: ${props => props.theme.breakpoints[2]}) {
       margin-block-end: 2rem;
     }
-  `
+  `;
   const PlatformBox = styled.div`
     width: 290px;
     height: 250px;
@@ -83,7 +83,7 @@ const Services = () => {
     background-image: url(${platform});
     background-size: cover;
 
-    @media (max-width: ${props => props.theme.breakpoints[2]}) { 
+    @media (max-width: ${props => props.theme.breakpoints[2]}) {
       margin-block-end: 2rem;
     }
   `;
@@ -96,7 +96,7 @@ const Services = () => {
     background-image: url(${custom});
     background-size: cover;
 
-    @media (max-width: ${props => props.theme.breakpoints[2]}) { 
+    @media (max-width: ${props => props.theme.breakpoints[2]}) {
       margin-block-end: 2rem;
     }
   `;
@@ -128,8 +128,8 @@ const Services = () => {
     font-weight: 300;
     @media (max-width: ${props => props.theme.breakpoints[2]}) {
       width: 50px;
-      height: 50px; 
-      font-size: 1.5rem; 
+      height: 50px;
+      font-size: 1.5rem;
     }
   `;
 
@@ -147,7 +147,7 @@ const Services = () => {
     flex-direction: column;
     @media (max-width: ${props => props.theme.breakpoints[2]}) {
       padding-left: 0rem;
-    padding-right: 0rem;
+      padding-right: 0rem;
     }
   `;
 
@@ -165,16 +165,18 @@ const Services = () => {
       >
         <CustomHeader>Services</CustomHeader>
         <p>
-          You are an entrepreneur, owner of a small business or start-up and 
-          looking for beautiful designs for your visual front door?
-          Don't waste your time on UpWork or fiverr and hit me a message to meet
-          me in person so we can discuss your needs.
-          Let's design a beautiful logo, business card or/and website together to get
-          one step further with your business.
-          For Logo and Business Card Design I will work with the Adobe Cloud to provide
-          professional designs. Platform based websites will be build within Squarespace, 
-          WIX or Wordpress.com. To make your website even faster and more unique, I also
-          provide custom webdesins, where I build the website from scratch.
+          You are an entrepreneur, owner of a small business or start-up and
+          looking for beautiful designs for your visual front door? Don't waste
+          your time on UpWork or fiverr, just send me a message and meet me in
+          person so we can discuss your needs face to face. Let's design a
+          beautiful logo, business card or/and website together and get one step
+          further with your business. For Logo and Business Card Design I will
+          work with the Adobe Cloud to provide professional designs. Platform
+          based websites will be build within Squarespace, WIX or Wordpress.com.
+          To make your website even faster and more unique, I also provide
+          custom web designs, where I build a website from scratch. This
+          approach will reduce your hosting fees and create a fast and
+          responsive website based on the latest web technologies.
         </p>
       </AnimatedBox>
       <Wrapper>
@@ -185,10 +187,10 @@ const Services = () => {
           <TitleBox>Business Card</TitleBox>
         </BusinessBox>
         <PlatformBox alt="Platform based Webdesign">
-          <TitleBox>Platform Webdesign</TitleBox>
+          <TitleBox>Platform Web Design</TitleBox>
         </PlatformBox>
         <CustomBox alt="Custom Webdesign">
-          <TitleBox>Custom Webdesign</TitleBox>
+          <TitleBox>Custom Web Design</TitleBox>
         </CustomBox>
       </Wrapper>
       <AnimatedBox
@@ -203,30 +205,29 @@ const Services = () => {
           <Steps>
             First of all we schedule a Skype or face-to-face meeting, I want to
             know more about your business, products and services. Tell me about
-            your target audience and the difference to your competitors.
-            We cover pricing, time frames, questions you may have and all I need
-            from you to start my work.
+            your target audience and how you differentiate yourself from
+            competition. We cover pricing, time frames, questions you may have
+            and all I need from you to start working.
           </Steps>
           <Circle>2</Circle>
           <Steps>
-            Based on your provided information, visions and goals, I start to work on 
-            your individual project - either Graphic or
-            Webdesign. I will deliver the first design draft within the
-            disucussed time frame.
+            Based on the information provided, visions and goals, I start to
+            work on your individual project - either Graphic or Web Design. I
+            will deliver the first design draft within the discussed time frame.
           </Steps>
           <Circle>3</Circle>
           <Steps>
-            Now it's your turn, I need you to give me an honest feedback
-            on my work as you have to be happy with the design in the end. 
-            In a discussion we decide how you want me to go on.
-            Minor changes are included in the package, for mayor changes 
-            I will charge to my hourly rate. 
+            Now it's your turn, we will be doing a round of initial feedback and
+            will define action items for further changes on the site. Minor
+            changes are included in the package, for mayor changes I will charge
+            my hourly rate (only with your agreement).
           </Steps>
           <Circle>4</Circle>
           <Steps>
             After going through the last couple of changes, I will deliver the
-            final designs in file formats as requested. For every website projects,
-            I provide a walk through your website.
+            final designs in the file formats required. For every website
+            project, I provide a walk through of your website. From here on all
+            major changes will be charged at my hourly rate.
           </Steps>
         </WrapperWork>
         <PBox style={{ textAlign: "center" }} py={10} px={[6, 6, 8, 10]}>
